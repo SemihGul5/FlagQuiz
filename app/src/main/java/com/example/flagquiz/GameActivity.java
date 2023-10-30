f3ef57350b510773b9691d5af7415126bc746cd3
Semih Gül <semih.gul099@gmail.com>
2023-10-30 20:12:43 +0300

    timer

--- app/src/main/java/com/example/flagquiz/GameActivity.java
+++ app/src/main/java/com/example/flagquiz/GameActivity.java
@@ -3,7 +3,10 @@ package com.example.flagquiz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 
 import com.example.flagquiz.databinding.ActivityGameBinding;
@@ -17,23 +20,51 @@ public class GameActivity extends AppCompatActivity {
         binding=ActivityGameBinding.inflate(getLayoutInflater());
         View view=binding.getRoot();
         setContentView(view);
+        binding.progressBarGame.setProgress(100);
+        binding.progressBarGame.getProgressDrawable().setColorFilter(Color.parseColor("#70F155"), PorterDuff.Mode.SRC_IN);
+
+        new CountDownTimer(60000,1000) {
+            @Override
+            public void onTick(long l) {
+
+                long sc=l/1000;
+                // Geri sayım her saniye gerçekleştiğinde burası çalışır
+                binding.kalanSureText.setText("Kalan süre "+l/1000);
+                int progressBarValue = (int) (l / 1000); // Kalan süre saniye cinsinden
+                binding.progressBarGame.setProgress(progressBarValue);
+
+                if (sc<=60&&sc>=40){
+                    binding.progressBarGame.getProgressDrawable().setColorFilter(Color.parseColor("#70F155"), PorterDuff.Mode.SRC_IN);
+                    binding.kalanSureText.setTextColor(getColor(R.color.Yeşil));
+                } else if (sc<40&&sc>=20) {
+                    binding.progressBarGame.getProgressDrawable().setColorFilter(Color.parseColor("#FFEB3B"), PorterDuff.Mode.SRC_IN);
+                    binding.kalanSureText.setTextColor(getColor(R.color.Sarı));
+                }
+                else {
+                    binding.progressBarGame.getProgressDrawable().setColorFilter(Color.parseColor("#F44336"), PorterDuff.Mode.SRC_IN);
+                    binding.kalanSureText.setTextColor(getColor(R.color.Kırmızı));
+                }
+            }
+
+            @Override
+            public void onFinish() {
+                binding.kalanSureText.setText("Süre doldu!");
+                binding.progressBarGame.setProgress(0);
+
+            }
+        }.start();
+
+
 
 
     }
     @Override
     protected void onPause() {
         super.onPause();
-        // Uygulama arka plana atıldığında burada yapmak istediğiniz işlemleri gerçekleştirin.
         Intent intent= new Intent(GameActivity.this, MainActivity2.class);
+        startActivity(intent);
+        finish();
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        // Uygulama ön plana geldiğinde burada yapmak istediğiniz işlemleri gerçekleştirin.
-        // Örneğin, önceki durumu geri yükleyebilir veya oyununuzu yeniden başlatabilirsiniz.
-        Intent intent= new Intent(GameActivity.this, MainActivity2.class);
-
-    }
 
 }
--- app/src/main/java/com/example/flagquiz/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/flagquiz/ui/notifications/NotificationsFragment.java
@@ -5,19 +5,41 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.flagquiz.databinding.FragmentNotificationsBinding;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.Map;
 
 public class NotificationsFragment extends Fragment {
 
     private FragmentNotificationsBinding binding;
+    FirebaseAuth auth;
+    FirebaseUser user;
+    FirebaseFirestore firestore;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        auth=FirebaseAuth.getInstance();
+        user=FirebaseAuth.getInstance().getCurrentUser();
+        firestore=FirebaseFirestore.getInstance();
+    }
+
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         NotificationsViewModel notificationsViewModel =
                 new ViewModelProvider(this).get(NotificationsViewModel.class);
 
@@ -30,8 +52,36 @@ public class NotificationsFragment extends Fragment {
     }
 
     @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        getEmailAndUserName();
+    }
+
+    @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+    private void getEmailAndUserName() {
+        if (user != null) {
+            firestore.collection("Users").whereEqualTo("email", user.getEmail()).addSnapshotListener(new EventListener<QuerySnapshot>() {
+                @Override
+                public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                    if (error != null) {
+                        Toast.makeText(getContext(), error.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                    if (value != null) {
+                        for (DocumentSnapshot documentSnapshot : value.getDocuments()) {
+                            Map<String, Object> data = documentSnapshot.getData();
+
+                            String hak = (String) data.get("kalanHak");
+                            binding.textView5.setText("Kalan gönderim hakkı: "+hak);
+
+                        }
+                    }
+                }
+            });
+        }
     }
 }
